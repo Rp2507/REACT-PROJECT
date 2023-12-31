61ef1aacd43df3c09effee7075777bc96bfc2b0b
Rp2507 <riyaparmar2507@gmail.com>
2023-12-31 11:15:56 +0530

    pr.5,6 complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Detail from './Components/Detail';
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <Detail />
+      </div>
+    </>
+  );
+
+}
+
+export default App;
+
